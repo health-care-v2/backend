34b5cdc754e45aea6efacafb9126fc1c2b9435bf
ChangHoMoon <tony9911@naver.com>
2024-03-08 18:11:52 +0900

    #16 - 일기 수정 기능 구현

--- src/main/java/com/example/healthcare_v2/domain/diary/controller/DiaryController.java
+++ src/main/java/com/example/healthcare_v2/domain/diary/controller/DiaryController.java
@@ -1,15 +1,13 @@
 package com.example.healthcare_v2.domain.diary.controller;
 
 import com.example.healthcare_v2.domain.diary.controller.request.CreateDiaryRequest;
+import com.example.healthcare_v2.domain.diary.controller.request.UpdateDiaryRequest;
 import com.example.healthcare_v2.domain.diary.service.DiaryService;
 import com.example.healthcare_v2.global.utill.ResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -24,9 +22,20 @@ public class DiaryController {
     public ResponseEntity<ResponseDTO<Void>> create(
             @Valid @RequestBody CreateDiaryRequest request,
             Principal principal
-    ){
+    ) {
         Long userId = Long.valueOf(principal.getName());
         diaryService.create(request, userId);
+        return ResponseEntity.ok(ResponseDTO.ok());
+    }
+
+    @PutMapping("/{diaryId}")
+    public ResponseEntity<ResponseDTO<Void>> update(
+            @PathVariable Long diaryId,
+            @Valid @RequestBody UpdateDiaryRequest request,
+            Principal principal
+    ) {
+        Long userId = Long.valueOf(principal.getName());
+        diaryService.update(request, userId, diaryId);
         return ResponseEntity.ok(ResponseDTO.ok());
     }
 }
--- src/main/java/com/example/healthcare_v2/domain/diary/service/DiaryService.java
+++ src/main/java/com/example/healthcare_v2/domain/diary/service/DiaryService.java
@@ -1,12 +1,16 @@
 package com.example.healthcare_v2.domain.diary.service;
 
 import com.example.healthcare_v2.domain.diary.controller.request.CreateDiaryRequest;
+import com.example.healthcare_v2.domain.diary.controller.request.UpdateDiaryRequest;
+import com.example.healthcare_v2.domain.diary.entity.Diary;
+import com.example.healthcare_v2.domain.diary.exception.DiaryNotFoundException;
+import com.example.healthcare_v2.domain.diary.exception.UserNotMatchException;
 import com.example.healthcare_v2.domain.diary.repository.DiaryRepository;
 import com.example.healthcare_v2.domain.patient.entity.Patient;
 import com.example.healthcare_v2.domain.patient.service.PatientService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +20,24 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final PatientService patientService;
 
+    @Transactional(readOnly = true)
+    public Diary findById(Long diaryId) {
+        return diaryRepository.findById(diaryId)
+                .orElseThrow(DiaryNotFoundException::new);
+    }
+
     public void create(CreateDiaryRequest request, Long userId) {
         Patient patient = patientService.findById(userId);
         diaryRepository.save(request.toEntity(patient));
+    }
+
+    public void update(UpdateDiaryRequest request, Long userId, Long diaryId) {
+        Patient patient = patientService.findActivePatientById(userId);
+        Diary diary = findById(diaryId);
+
+        if (!diary.getPatient().equals(patient)) {
+            throw new UserNotMatchException();
+        }
+        diary.update(request.toEntity(patient));
     }
 }
