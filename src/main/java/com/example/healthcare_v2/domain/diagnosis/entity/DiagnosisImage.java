35f6ca7c2ab70417cfd081470bdacd093fc82405
kmindev <kangkm0324@gmail.com>
2024-03-17 16:41:55 +0900

    #17 - entity 수정
    * 진단이미지 엔티티는 진단 엔티티 매핑관계가 빠져있어 매핑시켜줌.
    * 진단엔티티에서 @OneToMany 관계 필드는 nullPointException 발생할 수 있으므로 필드에서 초기화 해줌.

--- src/main/java/com/example/healthcare_v2/domain/diagnosis/entity/DiagnosisImage.java
+++ src/main/java/com/example/healthcare_v2/domain/diagnosis/entity/DiagnosisImage.java
@@ -1,11 +1,7 @@
 package com.example.healthcare_v2.domain.diagnosis.entity;
 
 import com.example.healthcare_v2.global.common.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
@@ -16,7 +12,12 @@ public class DiagnosisImage extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 300)
+    @Column(length = 1000)
     private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diagnosis_id")
+    private Diagnosis diagnosis;
+
 }
 
