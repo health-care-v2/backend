e5dd201d6a0d0a5144f4f71431d8d283d8aa335a
kmindev <kangkm0324@gmail.com>
2024-03-24 22:31:01 +0900

    #53 - 불필요한 메서드 제거
     soft delete로 수정한 다음 사용하지 않으므로 메서드 제거

--- src/main/java/com/example/healthcare_v2/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/healthcare_v2/domain/reservation/repository/ReservationRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    void deleteByIdAndPatient_Id(Long reservationId, Long patientId);
-
     Page<Reservation> findAllByDeletedAtIsNull(Pageable pageable);
 }
